a939cc826e2caaa99892afd573ccaf33155fac71
Daniel Kuncik <dkuncik@gmail.com>
2020-10-10 13:13:17 -0400

    create a very simple home page with a form; add handlebars and view engine and redis client

--- index.js
+++ index.js
@@ -1,13 +1,48 @@
 
 const express = require('express');
+const redis = require('redis');
+const hbs = require('express-hbs');
+const bodyParser = require('body-parser');
+
 
 let app = express();
+
+app.use(
+    bodyParser.urlencoded({
+        extended: true,
+    })
+);
+app.use(bodyParser.json());
+
+app.set('view engine', 'hbs');
+app.set('views', [__dirname + '/views']);
+app.engine('hbs', hbs.express4({
+    defaultView: 'default.hbs',
+    partialsDir: __dirname + '/views/partials',
+    layoutsDir: __dirname + '/views/layouts'
+}));
+
+let client = redis.createClient();
+client.on("error",(error) => {
+    throw error
+});
 
 const port = process.env.PORT || 3000;
 
 app.get('/',(req,res) => {
-    res.send('hello world');
+    res.render('home.hbs', {
+        layout: 'default'
+    });
 });
+
+app.post('/',(req, res) => {
+    console.log(req.body.key);
+    console.log(req.body.value);
+    res.render('home.hbs', {
+        layout: 'default'
+    });
+});
+
 
 app.listen(port, () => {
     console.log('server listening');
